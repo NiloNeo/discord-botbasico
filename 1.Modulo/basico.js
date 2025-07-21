ef8d1fe3d8dcb0b46740e6130f4d90fb1180d175
illika <137735257+SJPV@users.noreply.github.com>
2025-07-21 22:51:03 +0000

    feat: Uso del Evento messageCrate

--- 1.Modulo/basico.js
+++ 1.Modulo/basico.js
@@ -1,10 +1,19 @@
 import { Client, GatewayIntentBits } from "discord.js";
 
 export const client = new Client({
-    intents: [GatewayIntentBits.Guilds]
+    intents: [
+        GatewayIntentBits.Guilds, // slash commands
+        GatewayIntentBits.GuildMessages, // recibir mensajes
+        GatewayIntentBits.MessageContent // leer contenido de mensajes
+    ]
 });
 
 client.once('ready', () => {
     console.log("Bot listo", client.user.tag);
+});
+
+client.on('messageCreate', (message) => {
+    //console.log(message);
+    if (message.content === '!illika') message.reply('Este es el comando illika, devolviendo respuesta desde el backend');
 });
 
